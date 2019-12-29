5269711a2b87bca692cff91a9174d6961e66c1f0
rachhek <rachhek@gmail.com>
2019-12-30 00:30:23 +0200

    added circular effect

--- src/App.js
+++ src/App.js
@@ -6,10 +6,28 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={potrait} id="potrait" className="fade-in" alt="rachhek" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div class="outer-circle1">
+          <img src={potrait} id="potrait" className="fade-in" alt="rachhek" />
+        </div>
+        {/* <div class="ferris-wheel">
+          <div class="wheel-wrap">
+            <div class="wheels">
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+        </div> */}
+        <div className="orbit1">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="orbit2">
+          <div></div>
+          <div></div>
+        </div>
       </header>
     </div>
   );
