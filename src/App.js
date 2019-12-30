9f26007b7063afb37dc1884844e4c49c0147ff38
rachhek <rachhek@gmail.com>
2019-12-31 01:58:23 +0200

    added parallax effects to svgs

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Links from "./links/links";
 import "./App.css";
 import Parallax from "parallax-js";
 import note1 from "./images/note1.svg";
+import note2 from "./images/note2.svg";
+import note3 from "./images/note3.svg";
 import sun from "./images/sun.svg";
 
 import me from "./images/me.jpg";
@@ -20,9 +22,12 @@ function App() {
         <Links />
       </div>
       <div id="para">
-        <img data-depth="0.4" src={note1} id="note1" />
-        <img data-depth="2" src={note1} id="note2" />
-        <img data-depth="3" src={sun} id="sun1" />
+        <img data-depth="0.4" src={note1} id="note1_1" />
+        <img data-depth="1" src={note1} id="note1_2" />
+        <img data-depth="1" src={note2} id="note2_1" />
+        <img data-depth="1" src={note3} id="note3_1" />
+        <img data-depth="1" src={note3} id="note3_2" />
+        <img data-depth="1" src={sun} id="sun1" />
       </div>
     </div>
   );
